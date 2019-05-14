9de20c002902b4f91f85c9bb1aa97a32cc0525fe
Marko Jakic <jakicmeister@gmail.com>
2019-05-14 13:12:11 +0200

    build from cli and add cli options

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const fs = require('fs-extra')
+const program = require('commander')
+const { spawn } = require('child_process')
+const colors = require('colors')
+
+program
+  .option('-n, --name <name>', 'name of your project', 'react-struct')
+
+program.parse(process.argv)
+const projectPath = `${process.cwd()}/${program.name}`
+
+const cp = spawn('cp', ['-r', './package', projectPath])
+
+cp.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+})
+
+cp.on('close', (code) => {
+  const pjson = fs.readFileSync('./package/package.json.tmpl', 'utf8')
+  const par = JSON.parse(pjson)
+  par.name = program.name
+  fs.writeFileSync(`${projectPath}/package.json`, JSON.stringify(par, null, 2))
+  console.log(`Created project with name ${program.name}.`.green)
+  spawn('rm', [`${projectPath}/package.json.tmpl`])
+  console.log('Now install NPM modules: '.dim, `cd ${program.name} && npm i`.yellow.bold)
+  console.log(colors.gray(`Child process exited with code ${code}.`))
+});
